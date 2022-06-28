ea8dea648a64d1572ba588519ac8170f5208ae97
lol500q <mohmmadayesh22@gmail.com>
2022-06-28 14:48:12 +0300

    sigining port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/imageurl',(req,res)=>image.handleAPICall(req,res));
 bcrypt.hash("bacon", null, null, function(err, hash) {
     // Store hash in your password DB.
 });
-app.listen(3000,(()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT||3000,(()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 }));
 
