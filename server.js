95ee866ecf864beaf2b0b90ee7410192f406409e
lol500q <mohmmadayesh22@gmail.com>
2022-06-28 17:28:58 +0300

    fixing api

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/',(req,res)=>{
 app.post('/signin',(req,res)=>signin.handleSignin(req,res,db,bcrypt));
 app.post('/register',(req,res)=>register.handleRegister(req,res,db,bcrypt));
 app.get('/profile/:id',(req,res)=>profile.handleProfile(req,res,db));
-app.put('/image',(req,res)=>image.handleImage(req,res,db));
-app.post('/imageurl',(req,res)=>image.handleAPICall(req,res));
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 bcrypt.hash("bacon", null, null, function(err, hash) {
     // Store hash in your password DB.
 });
